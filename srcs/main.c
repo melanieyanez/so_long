850e10b3d2b2e281bbe17b1032b40129003e5032
Melanie Yanez <melanieyanez@gmail.com>
2023-08-20 23:01:40 +0200

    end of the end

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: melanieyanez <melanieyanez@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 17:52:36 by melanieyane       #+#    #+#             */
-/*   Updated: 2023/08/20 20:14:39 by melanieyane      ###   ########.fr       */
+/*   Updated: 2023/08/20 20:55:33 by melanieyane      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,11 @@ int	main(int argc, char **argv)
 	vars.wdw = mlx_new_window(vars.mlx, vars.map.map_x * SIZE, vars.map.map_y * SIZE + SIZE, "Forest Quest");
 	vars.img.img = mlx_new_image(vars.mlx, vars.map.map_x * SIZE, vars.map.map_y * SIZE);
 	vars.img.addr = mlx_get_data_addr(vars.img.img, &vars.img.bits_per_pixel, &vars.img.line_length, &vars.img.endian);
-	//drawing_updated(&vars);
 	mlx_hook(vars.wdw, 2, 0, key_actions, &vars);
 	mlx_hook(vars.wdw, 17, 0, close_window, &vars);
 	img_loading(&vars);
 	mlx_loop_hook(vars.mlx, render, &vars);
 	mlx_loop(vars.mlx);
-	//mlx_destroy_image(vars.mlx, vars.img.img);
+	mlx_destroy_image(vars.mlx, vars.img.img);
 	return (0);
 }
--- srcs/render.c
+++ srcs/render.c
@@ -6,19 +6,14 @@
 /*   By: melanieyanez <melanieyanez@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 10:18:39 by melanieyane       #+#    #+#             */
-/*   Updated: 2023/08/20 20:51:23 by melanieyane      ###   ########.fr       */
+/*   Updated: 2023/08/20 23:00:51 by melanieyane      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-void	draw_player(t_vars *vars, int i, int j)
+void	draw_player(t_vars *vars)
 {
-	if (vars->player.pos_x == -1 && vars->player.pos_y == -1)
-	{
-		vars->player.pos_x = j * SIZE;
-		vars->player.pos_y = i * SIZE;
-	}
 	if (vars->rev == 0)
 	{
 		mlx_put_image_to_window(vars->mlx, vars->wdw, vars->player.img.img, vars->player.pos_x, vars->player.pos_y);
@@ -35,8 +30,8 @@ void	img_loading(t_vars *vars)
 	vars->wall.img = mlx_png_file_to_image(vars->mlx, "imgs/tree-variations2.png", &vars->wall.line_length, &vars->wall.endian);
 	vars->player.img.img = mlx_png_file_to_image(vars->mlx, "imgs/fox.png", &vars->player.img.line_length, &vars->player.img.endian);
 	vars->player.img_rev.img = mlx_png_file_to_image(vars->mlx, "imgs/fox_rev.png", &vars->player.img_rev.line_length, &vars->player.img_rev.endian);
-	vars->collectible.img = mlx_png_file_to_image(vars->mlx, "imgs/rasp.png", &vars->collectible.line_length, &vars->collectible.endian);
-	vars->exit.img = mlx_xpm_file_to_image(vars->mlx, "imgs/exit.xpm", &vars->exit.line_length, &vars->exit.endian);
+	vars->collectible.img = mlx_png_file_to_image(vars->mlx, "imgs/rasp2.png", &vars->collectible.line_length, &vars->collectible.endian);
+	vars->exit.img = mlx_png_file_to_image(vars->mlx, "imgs/waterfall.png", &vars->exit.line_length, &vars->exit.endian);
 	if (vars->background.img == NULL || vars->wall.img == NULL || vars->player.img.img == NULL || vars->collectible.img == NULL || vars->exit.img == NULL)
 	{
 		printf("Error!\nImage couldn't be loaded.\n");
@@ -67,12 +62,18 @@ void	drawing_updated(t_vars *vars)
 			}
 			if (vars->map_array[i][j] == '1')
 				mlx_put_image_to_window(vars->mlx, vars->wdw, vars->wall.img, j * SIZE, i * SIZE);
-			else if (vars->map_array[i][j] == 'C')
-				mlx_put_image_to_window(vars->mlx, vars->wdw, vars->collectible.img, j * SIZE, i * SIZE);
-			else if (vars->map_array[i][j] == 'E')
+			if (vars->map_array[i][j] == 'C')
+				mlx_put_image_to_window(vars->mlx, vars->wdw, vars->collectible.img, j * SIZE + 10, i * SIZE + 12);
+			if (vars->map_array[i][j] == 'E')
 				mlx_put_image_to_window(vars->mlx, vars->wdw, vars->exit.img, j * SIZE, i * SIZE);
-			else if (vars->map_array[i][j] == 'P')
-				draw_player(vars, i, j);
+			if (vars->map_array[i][j] == 'P')
+			{
+				if (vars->player.pos_x == -1 && vars->player.pos_y == -1)
+				{
+					vars->player.pos_x = j * SIZE;
+					vars->player.pos_y = i * SIZE;
+				}
+			}
 			j ++;
 		}
 		i ++;
@@ -106,10 +107,10 @@ void	game_data(t_vars *vars)
 	mlx_string_put(vars->mlx, vars->wdw, 125, vars->map.res_y + 30, WHITE, ft_itoa(vars->collected));
 	mlx_string_put(vars->mlx, vars->wdw, vars->map.res_x - 150, vars->map.res_y + 30, WHITE, "number of moves:");
 	mlx_string_put(vars->mlx, vars->wdw, vars->map.res_x - 30, vars->map.res_y + 30, WHITE, ft_itoa(vars->moves));
-	if (vars->collected == vars->to_collect)
-	{
-		mlx_string_put(vars->mlx, vars->wdw, vars->map.res_x / 2 - 150, vars->map.res_y + 30, WHITE, "You got all the fruits, go for the exit!");
-	}
+	if (vars->collected < vars->to_collect)
+		mlx_string_put(vars->mlx, vars->wdw, vars->map.res_x / 2 - 150, vars->map.res_y + 30, WHITE, "Hurry! You need to grab all the fruits!");
+	else if (vars->collected == vars->to_collect)
+		mlx_string_put(vars->mlx, vars->wdw, vars->map.res_x / 2 - 150, vars->map.res_y + 30, WHITE, "You got all the fruits, go rest at the waterfall!");
 }
 
 int	render(t_vars *vars)
@@ -117,6 +118,7 @@ int	render(t_vars *vars)
 	mlx_clear_window(vars->mlx, vars->wdw);
 	draw_background(vars);
 	drawing_updated(vars);
+	draw_player(vars);
 	game_data(vars);
 	return (0);
 }
